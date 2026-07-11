7c0b5f90f619023a87fe904d0880a7ae46730c60
Mingyu Hsia <myhsia@outlook.com>
2026-07-11 17:22:03 +0800

    Resolve Git blame pollution

---
+++ build.lua
@@ -0,0 +1,25 @@
+--[==========================================[--
+         L3BUILD FILE FOR "PHY5047@QMB"
+--]==========================================]--
+
+module           = "PHY5047@QMB"
+version          = "2025-11-23"
+docsuppdirs      = {"context", "media"}
+unpacksuppfiles  = {"*.bib"}
+textfiles        = {"*.md", "LICENSE", "*.lua"}
+typesetexe       = "latexmk"
+typesetfiles     = {module .. ".tex"}
+typesetopts      = "-pdf -interaction=nonstopmode"
+typesetruns      = 1
+
+function docinit_hook()
+  for _,glob in pairs(docsuppdirs) do
+    run(currentdir, "cp -r " .. glob .. " " .. typesetdir)
+  end
+  return 0
+end
+function tex(file,dir,cmd)
+  dir = dir or "."
+  cmd = cmd or typesetexe .. " " .. typesetopts
+  return run(dir, cmd .. file)
+end
